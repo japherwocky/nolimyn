e6bc8e144d18821789a13c6afd8badf84b79855c
Japhy <japherwocky@pearachute.com>
2015-03-22 00:13:45 +0000

    update server build from bonquifa

--- MkServer.sh
+++ MkServer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-java -Xmx2G -XX:MaxPermSize=512M -jar bin/modpack.jar nogui --installServer;
-mv mods/journeymap-server.jar.server-only mods/journeymap-server.jar
-rm mods/JourneyMap5.0.1_FairPlay_MC1.7.10.jar
+java -jar bin/forge-1.7.10-10.13.2.1291-installer.jar nogui --installServer
+mv mods/JourneyMapServer1.0.4_MC1.7.10.jar.server-only mods/journeymap-server.jar
+rm mods/JourneyMap5.0.1_Unlimited_MC1.7.10.jar
+rm mods/BetterTitleScreen-1.7.10-1.1.jar
