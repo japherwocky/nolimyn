acb68fa7b53c6a126459d6e5478a89f44092f6ef
Japherwocky <japhy@pearachute.com>
2015-10-20 10:06:43 -0500

    update server build script to use mods-server

--- MkServer.sh
+++ MkServer.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 java -jar bin/forge-1.7.10-10.13.2.1291-installer.jar nogui --installServer
-mv mods/JourneyMapServer1.0.4_MC1.7.10.jar.server-only mods/journeymap-server.jar
-rm mods/JourneyMap5.0.1_Unlimited_MC1.7.10.jar
-rm mods/BetterTitleScreen-1.7.10-1.1.jar
+mv mods-server/* mods/
