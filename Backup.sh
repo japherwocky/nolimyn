67a7761889c344e89c57b35d3873a193652a67a1
japherwocky <japherwocky@charlotte>
2015-10-22 19:51:54 +0000

    notes on backups

--- Backup.sh
+++ Backup.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 TIME=`date +%Y-%m-%d_%H-%M-%S`
+
+# add to crontab like:
+# 0 * * * * sh /opt/nolimyn/Backup.sh
+
+# copy into /tmp/ so files don't change while we're packing them up
 cp -r /opt/nolimyn/world /tmp/worldbackup
+
+# and put into backups folder
 tar -cvzf /opt/nolimyn/backups/world-$TIME.tar.gz /tmp/worldbackup
+
+# TODO - wipe backups older than X days
