8ecebc1885bc7cbdefabb019a3ced3e1c9a50b4a
japherwocky <japherwocky@charlotte>
2016-05-29 19:48:56 +0000

    update server scripts to use most recent forge

--- LaunchServer.sh
+++ LaunchServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx3G -Xmx3G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:MaxGCPauseMillis=250 -XX:ParallelGCThreads=4 -XX:ConcGCThreads=4 -XX:+AggressiveOpts -server -jar forge-1.7.10-10.13.3.1403-1.7.10-universal.jar nogui
+java -Xmx3G -Xmx3G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:MaxGCPauseMillis=250 -XX:ParallelGCThreads=4 -XX:ConcGCThreads=4 -XX:+AggressiveOpts -server -jar forge-1.8.9-11.15.1.1855-universal.jar nogui
--- MkServer.sh
+++ MkServer.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-java -jar bin/forge-1.7.10-10.13.4.1448-1.7.10-installer.jar nogui --installServer
-# mv mods/JourneyMapServer1.0.4_MC1.7.10.jar.server-only mods/journeymap-server.jar
-rm mods/journeymap-1.7.10-5.1.0-unlimited.jar
-# rm mods/BetterTitleScreen-1.7.10-1.1.jar
+java -jar bin/forge-1.8.9-11.15.1.1855-installer.jar nogui --installServer
