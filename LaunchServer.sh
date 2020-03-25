13e0a6413fe89a5163c35f31cb1bbd930d551ff3
japherwocky <japherwocky@charlotte>
2020-03-25 20:48:35 +0000

    tweak server launch scripts

--- LaunchServer.sh
+++ LaunchServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx3G -Xmx3G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:MaxGCPauseMillis=250 -XX:ParallelGCThreads=4 -XX:ConcGCThreads=4 -XX:+AggressiveOpts -server -jar forge-1.8.9-11.15.1.1855-universal.jar nogui
+java -Xmx3G -Xmx3G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:MaxGCPauseMillis=250 -XX:ParallelGCThreads=4 -XX:ConcGCThreads=4 -XX:+AggressiveOpts -server -jar forge-latest-universal.jar nogui
--- MkServer.sh
+++ MkServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar bin/forge-1.8.9-11.15.1.1855-installer.jar nogui --installServer
+java -jar bin/forge-latest-installer.jar nogui --installServer
