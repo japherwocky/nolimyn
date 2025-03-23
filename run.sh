01a4e0a0ba1575abf945ee81e053f8fcee9cd4e1
Japherwocky <japhy@pearachute.com>
2025-03-23 17:49:48 +0000

    paper updates, server data

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.2.19/unix_args.txt "$@"
+java -jar paper-1.21.4-214.jar nogui
