a8ffd62ab5afbc1cbdf921eed9c0c57ca500e2d5
Japhy Bartlett <japhy@pearachute.com>
2025-04-05 18:55:16 -0400

    upgrade paper, add stone portals for resource world

--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@ REM Forge requires a configured set of both JVM and program arguments.
 REM Add custom JVM arguments to the user_jvm_args.txt
 REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
 REM  pass them to this script directly
-.\jdk-23.0.1\bin\java.exe @user_jvm_args.txt -jar ./paper-1.21.4-214.jar %*
+.\jdk-23.0.1\bin\java.exe @user_jvm_args.txt -jar ./paper-1.21.4-222.jar %*
 pause
